0c610a60e65beab1bbdd86ab640e73a12952fe83
Marien Krouwel <marienkrouwel@gmail.com>
2023-04-02 13:19:44 +0200

    using new simplesdk lib

--- translator.js
+++ translator.js
@@ -1,117 +1,136 @@
-//import { json } from "stream/consumers";
-import { MendixPlatformClient } from "mendixplatformsdk";
-import { projects, security, domainmodels, microflows, enumerations, texts } from "mendixmodelsdk";
+import { SimpleModel } from '../mendixsdk/build/index.js';
+import { DataType } from '../mendixsdk/build/lib/dict.js';
 import descriptor from "./descriptor.json" assert { type: "json" };
 descriptor.demomodel;
 const demomodel = (await import(descriptor.demomodel, { assert: { type: 'json' } })).default;
 console.log(demomodel);
-const workingCopy = await loadWorkingCopy();
-const model = await workingCopy.openModel();
-//model.importModuleMpk() TODO
-const [module, domainModel] = createModule();
-// transaction kinds
+const simpleModel = new SimpleModel();
+await simpleModel.openExistingApp(descriptor.appid);
+//await simpleModel.createAndOpenApp(descriptor.appname)
+await simpleModel.deleteModules(descriptor.defaultmodule, true);
+const simpleModule = simpleModel.addModule(descriptor.defaultmodule + (new Date()).toISOString());
+const [userRole] = await simpleModule.addModuleRoles({ User: "User" });
+simpleModule.addEntity("AssessmentResult", undefined, false).addBooleanAttribute("AssessmentResult");
+/*
+case "eventtype":
+            
+const entity = domainmodels.Entity.createIn(domainModel);
+entity.name = replaceWhiteSpace(factkind.name);
+//domainmodels.Generalization.createIn(entity).generalization = model.findEntityByQualifiedName("Transaction.Proposition"); TODO
+
+const assoc = domainmodels.Association.createIn(domainModel);
+assoc.name = replaceWhiteSpace(factkind.parameter);
+assoc.parent = entity;
+assoc.child = domainModel.entities.find(e => e.name == replaceWhiteSpace(factkind.parameter));
+break;
+*/
 // actor roles ...
-// fact kinds
-for (var i = 0; i < demomodel.factkinds.length; i++) {
-    var factkind = demomodel.factkinds[i];
-    if (factkind.type === "entitytype") {
-        const entity = domainmodels.Entity.createIn(domainModel);
-        entity.name = replaceWhiteSpace(factkind.name);
+let entities = {};
+let primitives = {};
+let enums = {};
+// entities first
+for (let factkind of demomodel.factkinds.filter(f => f.type == "entitytype")) {
+    console.log(factkind.name);
+    entities[factkind.name] = simpleModule.addEntity(factkind.name);
+}
+// transaction kinds
+for (let transaction of demomodel.transactionkinds) {
+    console.log(transaction.name);
+    switch (transaction.type) {
+        case "elementary":
+            const tkEntity = simpleModule.addEntity(transaction.productname);
+            for (let casekind of transaction.casekinds) {
+                tkEntity.addAssociation(entities[casekind], undefined);
+            }
+            entities[transaction.productname] = tkEntity;
+            break;
+        case "aggregate":
+            // nothing todo
+            break;
+        default: throw new Error(transaction.type + "not supported");
     }
-    else if (factkind.type === "valuetype") {
-        const enumE = enumerations.Enumeration.createIn(module);
-        enumE.name = replaceWhiteSpace(factkind.name);
-        factkind.values.split(", ").forEach(v => {
-            const enumV = enumerations.EnumerationValue.createIn(enumE);
-            enumV.name = replaceWhiteSpace(v);
-            texts.Text.createInEnumerationValueUnderCaption(enumV);
-            //TODO: need to set caption text here...
-        });
-    }
-    else if (factkind.type === "propertytype") {
-        const assoc = domainmodels.Association.createIn(domainModel);
-        assoc.name = replaceWhiteSpace(factkind.name);
-        assoc.parent = domainModel.entities.find(e => e.name == replaceWhiteSpace(factkind.domain));
-        assoc.child = domainModel.entities.find(e => e.name == replaceWhiteSpace(factkind.range));
-    }
-    else if (factkind.type === "attributetype") {
-        const attr = domainmodels.Attribute.createIn(domainModel.entities.find(e => e.name == replaceWhiteSpace(factkind.domain)));
-        attr.name = replaceWhiteSpace(factkind.name);
-        if (factkind.range == "datetime")
-            domainmodels.DateTimeAttributeType.createInAttributeUnderType(attr);
-        if (factkind.range == "bool")
-            domainmodels.BooleanAttributeType.createInAttributeUnderType(attr);
-        if (factkind.range == "number")
-            domainmodels.IntegerAttributeType.createInAttributeUnderType(attr);
-        if (factkind.range == "money")
-            domainmodels.DecimalAttributeType.createInAttributeUnderType(attr);
-        else { // suppose enum
-            // const enumT = domainmodels.EnumerationAttributeType.createInAttributeUnderType(attr);
-            // need to set the right enum but below doesnt work...
-            // enumT.enumeration = model.findEnumerationByQualifiedName(descriptor.defaultmodule + "." + replaceWhiteSpace(factkind.range));
-        }
-    }
-    else if (factkind.type === "eventtype") {
-        const entity = domainmodels.Entity.createIn(domainModel);
-        entity.name = replaceWhiteSpace(factkind.name);
-        //domainmodels.Generalization.createIn(entity).generalization = model.findEntityByQualifiedName("Transaction.Proposition"); TODO
-        const assoc = domainmodels.Association.createIn(domainModel);
-        assoc.name = replaceWhiteSpace(factkind.parameter);
-        assoc.parent = entity;
-        assoc.child = domainModel.entities.find(e => e.name == replaceWhiteSpace(factkind.parameter));
-    }
-    else if (factkind.type === "derived") {
-        const microflow = microflows.Microflow.createIn(module);
-        microflow.name = "Calculate_" + replaceWhiteSpace(factkind.name);
-        microflows.StartEvent.createIn(microflow.objectCollection);
-        microflows.EndEvent.createIn(microflow.objectCollection);
-        const parameter = microflows.MicroflowParameterObject.createIn(microflow.objectCollection);
-        parameter.name = replaceWhiteSpace(factkind.parameter);
+}
+// other fact kinds
+for (let factkind of demomodel.factkinds) {
+    //console.info(entities)
+    console.log(factkind.name);
+    switch (factkind.type) {
+        case "entitytype":
+            //skip
+            break;
+        case "valuetype":
+            if (factkind.values) {
+                const newEnum = simpleModule.addEnumeration(factkind.name);
+                factkind.values.split(", ").forEach(v => {
+                    newEnum.addEnumerationValue(v, v);
+                });
+                enums[factkind.name] = newEnum;
+            }
+            else
+                primitives[factkind.name] = factkind.primitive;
+            break;
+        case "propertytype":
+            entities[factkind.domain].addAssociation(entities[factkind.range], undefined, undefined, undefined, factkind.name);
+            break;
+        case "attributetype":
+            if (enums[factkind.range])
+                entities[factkind.domain].addEnumAttribute(factkind.name, enums[factkind.range]);
+            else
+                switch (primitives[factkind.range]) {
+                    case "datetime":
+                        entities[factkind.domain].addDateTimeAttribute(factkind.name);
+                        break;
+                    case "bool":
+                        entities[factkind.domain].addBooleanAttribute(factkind.name);
+                        break;
+                    case "integer":
+                        entities[factkind.domain].addIntegerAttribute(factkind.name);
+                        break;
+                    case "number":
+                        entities[factkind.domain].addDecimalAttribute(factkind.name);
+                        break;
+                    default: throw new Error(factkind.range + "not supported");
+                }
+            break;
+        case "derived":
+            const newMF = simpleModule.addMicroflow("Calculate_" + factkind.name);
+            for (let parameter of factkind.parameters) {
+                if (entities[parameter])
+                    newMF.addMicroflowParameter(parameter, DataType.Entity, entities[parameter]._entity);
+                else
+                    switch (primitives[parameter]) {
+                        case "datetime":
+                            newMF.addMicroflowParameter(parameter, DataType.DateTime);
+                            break;
+                        case "bool":
+                            newMF.addMicroflowParameter(parameter, DataType.Boolean);
+                            break;
+                        case "integer":
+                            newMF.addMicroflowParameter(parameter, DataType.Integer);
+                            break;
+                        case "number":
+                            newMF.addMicroflowParameter(parameter, DataType.Decimal);
+                            break;
+                        default: throw new Error(primitives[parameter] + "not supported");
+                    }
+                newMF.addEndEvent(newMF.getStartEvent(), 1);
+            }
+            break;
+        default: throw new Error(factkind.type + "not supported");
     }
 }
 // action rules
-for (var i = 0; i < demomodel.actionrules.length; i++) {
-    var actionrule = demomodel.actionrules[i];
+for (let actionrule of demomodel.actionrules) {
     // process event part...
-    const microflow = microflows.Microflow.createIn(module);
-    microflow.name = "AssessTruth_" + actionrule.actorrole + "_" + replaceWhiteSpace(actionrule.name);
-    microflows.StartEvent.createIn(microflow.objectCollection);
-    microflows.EndEvent.createIn(microflow.objectCollection);
+    const newMF = simpleModule.addMicroflow("AssessTruth_" + actionrule.actorrole + "_" + actionrule.id);
+    newMF.addEndEvent(newMF.getStartEvent(), 1);
+    //newMF.addMicroflowObjectParameter("Account", entityAccountToValidate, "The object to validate");
     // process response part...
 }
 // OIVs
-for (var i = 0; i < demomodel.oivs.length; i++) {
-    var oiv = demomodel.oivs[i];
-    const entity = domainmodels.Entity.createIn(domainModel);
-    entity.name = replaceWhiteSpace(oiv.name);
-    const attr = domainmodels.Attribute.createIn(entity);
-    attr.name = "Name";
+for (let oiv of demomodel.oivs) {
+    const oivEntity = simpleModule.addEntity(oiv.name);
+    oivEntity.addStringAttribute("Name");
 }
-await model.flushChanges();
-await workingCopy.commitToRepository("trunk", { commitMessage: "Generated app v1" });
-async function loadWorkingCopy() {
-    const client = new MendixPlatformClient();
-    var app;
-    // @ts-ignore
-    if (descriptor.appid != null)
-        // @ts-ignore
-        app = client.getApp(descriptor.appid);
-    else
-        app = await client.createNewApp(descriptor.appname + Date.now());
-    return app.createTemporaryWorkingCopy("trunk");
-}
-function createModule() {
-    // try to load module if it exists, but run into IModule vs Module issues...
-    //const defaultModule = model.findModuleByQualifiedName(replaceWhiteSpace(descriptor.defaultmodule));
-    //if (defaultModule != null) {
-    //    return [defaultModule, defaultModule.domainModel];
-    //} 
-    const module = projects.Module.createIn(model.allProjects()[0]);
-    module.name = replaceWhiteSpace(descriptor.defaultmodule);
-    const moduleSecurity = security.ModuleSecurity.createIn(module);
-    const domainModel = domainmodels.DomainModel.createIn(module);
-    return [module, domainModel];
-}
-function replaceWhiteSpace(s) {
-    return s.replace(/\s/g, '_');
-}
+await simpleModel.commit("Generated app v1");
+process.exit();
